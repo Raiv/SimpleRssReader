dd9afa2acf28765c1e52d42ea6eedc804f7356ef
Ivan Rogozhev <raiv.ixx@gmail.com>
2014-09-12 16:27:57 +0400

    project setup

---
+++ src/ru/raiv/simplerssreader/ActivityMain.java
@@ -0,0 +1,42 @@
+package ru.raiv.simplerssreader;
+
+import ru.raiv.angrycockroaches.R;
+import ru.raiv.angrycockroaches.VideoOpenGLRenderer;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+
+
+public class ActivityMain extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		setContentView(R.layout.activity_main);
+		
+
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+	}
+}
